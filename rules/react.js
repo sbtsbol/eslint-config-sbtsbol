95d47add799453633025b85131836501ff6b8ad8
Stanislav Cherenkov <SACherenkov.SBT@sberbank.ru>
2017-03-02 18:38:32 +0300

    Allow .spec.js to have jsx code

--- rules/react.js
+++ rules/react.js
@@ -252,7 +252,7 @@ module.exports = {
 
         // only .jsx files may have JSX
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-        'react/jsx-filename-extension': ['error', { extensions: ['.jsx'] }],
+        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.spec.js'] }],
 
         // prevent accidental JS comments from being injected into JSX as text
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-comment-textnodes.md
