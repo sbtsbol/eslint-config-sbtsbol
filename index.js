1e75b7dfecf81553415e9e2e8579ab26e0232379
Кузнецов Дмитрий Михайлович <DMKuznetsov.SBT@sberbank.ru>
2017-09-14 11:23:42 +0300

    remove deprecationWarning: [eslint] The 'ecmaFeatures'

--- index.js
+++ index.js
@@ -19,20 +19,6 @@ module.exports = {
         './rules/import',
         './rules/a11y',
     ].map(require.resolve),
-    ecmaFeatures: {
-        arrowFunctions: true,
-        blockBindings: true,
-        classes: true,
-        defaultParams: true,
-        destructuring: true,
-        jsx: true,
-        modules: true,
-        objectLiteralShorthandMethods: true,
-        objectLiteralShorthandProperties: true,
-        spread: true,
-        templateStrings: true,
-        experimentalObjectRestSpread: true
-    },
     parserOptions: {
         ecmaVersion: 2017,
         sourceType: 'module',
